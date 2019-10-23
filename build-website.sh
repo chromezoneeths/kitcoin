b9a5581e1536e82439f84182c36b2791edc797e8
Tucker Carlson-Huber <tucker61761@gmail.com>
2019-10-23 10:20:23 -0500

    Fixed some bugs with the build script

--- build-website.sh
+++ build-website.sh
@@ -3,8 +3,14 @@ git clone https://git.spaghet.us/ethsdev/kitcoin-app-cordova ../kitcoin-app-cord
 ORIGINAL_PATH=$(pwd)
 cd ../kitcoin-app-cordova
 npm install
+echo "ATTENTION: You'll need to edit config.json to match where you're deploying this."
+sleep 2
+cp www/config.json www/config.json.bkp
+nano www/config.json
 cordova build browser
+mv www/config.json.bkp www/config.json
 cd $ORIGINAL_PATH
+rm -r public
 mkdir -p public
 cp -r ../kitcoin-app-cordova/platforms/browser/www/* public
 echo "Done, ready for deployment."
