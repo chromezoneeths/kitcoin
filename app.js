5f5327ab1d3b09a7e5bf0658126e048cde736d69
Albert Portnoy <albert.portnoy@gmail.com>
2020-01-29 21:32:56 -0600

    Switched view engine, preparing for authentication
    • View engine is now Handlebars
    • Installed some basic dependencies
    • Basic home/login/dashboard/logout pages setup (NO FUNCTIONAL LOG IN / LOG OUT SYSTEM)

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
+/* jshint -W119*/
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var exphbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var dashboardRouter = require('./routes/dashboard');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.listen(5000, () => {
+  console.log('KitCoin is listening on port 5000.');
+});
 
-app.use(logger('dev'));
+
+// view engine setup
+app.engine('hbs', exphbs({
+  extname: 'hbs',
+  defaultLayout: false
+}));
+app.set('view engine', 'hbs');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/dashboard', dashboardRouter);
+app.use('/login', loginRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/login.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('login', {
+        title: 'Login',
+        layout: false
+    });
+});
+
+router.get('/logout', function (req, res, next) {
+    res.render('logout', {
+        title: 'Log Out',
+        layout: false
+    });
+});
+
+module.exports = router;
