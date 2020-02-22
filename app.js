d008afca780f5a91ca29c49cd11890ecf8054bfb
Tucker Carlson-Huber <tucker61761@gmail.com>
2020-02-21 19:36:40 -0600

    Moved google oauth stuff to a separate file.

--- app.js
+++ app.js
@@ -23,39 +23,22 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 const google = require('./google.js');
 const fs = require('fs');
-const oauthRouter = express.Router()
-oauthRouter.get('/oauthstage1', (req,res,next)=>{
-  google.callback(req,res,'/oauthstage1')
-})
-oauthRouter.get('/oauthstage2', (req,res,next)=>{
-  google.callback(req,res,'/oauthstage2')
-})
-oauthRouter.get('/oauthstage3', (req,res,next)=>{
-  console.log(req.url);
-  google.callback(req,res,req.url)
-})
-oauthRouter.get('/stage1.js', (req,res,next)=>{
-  res.end(fs.readFileSync('clientjs/stage1.js'))
-})
-oauthRouter.get('/stage2.js', (req,res,next)=>{
-  res.end(fs.readFileSync('clientjs/stage2.js'))
-})
-app.use('/oauth', oauthRouter)
+app.use('/oauth', oauthRouter);
 
 // Catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+	next(createError(404));
 });
 
 // Error handler
 app.use((err, req, res, next) => {
 	// Set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 	// Render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 // Bits added to the end for the backend, probably awful and wrong.
@@ -79,29 +62,29 @@ async function session (ws, req) {
 	console.log('Got new connection');
 	const auth = await google.prepare(ws);
 	const peopleAPI = googleapis.people({
-    version: 'v1',
-    auth: auth.auth
+		version: 'v1',
+		auth: auth.auth
 	});
 	const classroomAPI = googleapis.classroom({
-    version: 'v1',
-    auth: auth.auth
+		version: 'v1',
+		auth: auth.auth
 	});
 	const user = await peopleAPI.people.get({
-    resourceName: 'people/me',
-    personFields: 'emailAddresses,names'
+		resourceName: 'people/me',
+		personFields: 'emailAddresses,names'
 	});
-  console.log(`RECORDS, LOGGING: User ${user.data.names[0].displayName} has connected with email ${user.data.emailAddresses[0].value}.`);
+	console.log(`RECORDS, LOGGING: User ${user.data.names[0].displayName} has connected with email ${user.data.emailAddresses[0].value}.`);
 	const userQuery = await db.getUserByAddress(user.data.emailAddresses[0].value);
 	let userID; let address; let name; let admin;
 	const ping = setInterval(() => {
 		ws.send(JSON.stringify({ action: 'ping' }));
 	}, 500);
-  if (userQuery != undefined) {
+	if (userQuery != undefined) {
 		userID = userQuery.uuid;
 		address = userQuery.address;
 		name = userQuery.name;
 		admin = userQuery.role == 'admin';
-  } else {
+	} else {
 		userID = uuid();
 		admin = false;
 		await db.addUser(userID, user.data.emailAddresses[0].value, user.data.names[0].displayName).catch(error => {
@@ -109,186 +92,186 @@ async function session (ws, req) {
 		});
 		address = user.data.emailAddresses[0].value;
 		name = user.data.names[0].displayName;
-  }
+	}
 
-  ws.send(JSON.stringify({
+	ws.send(JSON.stringify({
 		action: 'ready',
 		name,
-    email: address,
-    balance: db.getBalance(userID)
+		email: address,
+		balance: db.getBalance(userID)
 	}));
 	ws.on('message', async stringMessage => {
 		const message = JSON.parse(stringMessage);
-    switch (message.action) {
+		switch (message.action) {
 			case 'getBalance': {
 				const balance = await db.getBalance(userID);
-        ws.send(JSON.stringify({
+				ws.send(JSON.stringify({
 					action: 'balance',
 					balance
 				}));
-        break;
-      }
+				break;
+			}
 
 			case 'sendCoin': {
 				const targetAddress = message.target;
 				let isBalanceSufficient;
 				let target;
-        await Promise.all([
-          new Promise(async (r, rj) => {
+				await Promise.all([
+					new Promise(async (r, rj) => {
 						const balance = await db.getBalance(userID).catch(rj);
 						isBalanceSufficient = balance > message.amount;
 						r();
-          }),
-          new Promise(async (r, rj) => {
+					}),
+					new Promise(async (r, rj) => {
 						target = await db.getUserByAddress(targetAddress).catch(rj);
 						r();
-          })
+					})
 				]);
-        console.log(isBalanceSufficient);
-        if (message.amount !== parseInt(message.amount, 10) || !/[A-Za-z0-9]*\@[A-Za-z0-9]*\.[a-z]{3}/.test(message.target)) {
-          ws.send(JSON.stringify({
+				console.log(isBalanceSufficient);
+				if (message.amount !== parseInt(message.amount, 10) || !/[A-Za-z0-9]*\@[A-Za-z0-9]*\.[a-z]{3}/.test(message.target)) {
+					ws.send(JSON.stringify({
 						action: 'sendResponse',
 						status: 'badInput'
 					}));
-        } else if (isBalanceSufficient && target != undefined) {
+				} else if (isBalanceSufficient && target != undefined) {
 					await db.addTransaction(userID, target[0], message.amount);
-          ws.send(JSON.stringify({
+					ws.send(JSON.stringify({
 						action: 'sendResponse',
 						status: 'ok'
 					}));
-        } else if (target == undefined) {
-          ws.send(JSON.stringify({
+				} else if (target == undefined) {
+					ws.send(JSON.stringify({
 						action: 'sendResponse',
 						status: 'nonexistentTarget'
 					}));
-        } else if (!isBalanceSufficient) {
-          ws.send(JSON.stringify({
+				} else if (!isBalanceSufficient) {
+					ws.send(JSON.stringify({
 						action: 'sendResponse',
 						status: 'insufficientBalance'
 					}));
-        }
+				}
 
-        break;
-      }
+				break;
+			}
 
 			case 'mintCoin': {
-        if (message.amount !== parseInt(message.amount, 10)) {
-          ws.send(JSON.stringify({
+				if (message.amount !== parseInt(message.amount, 10)) {
+					ws.send(JSON.stringify({
 						action: 'mintResponse',
 						status: 'badInput'
 					}));
-        } else if (admin) {
+				} else if (admin) {
 					await db.addTransaction('nobody', userID, message.amount);
-          ws.send(JSON.stringify({
+					ws.send(JSON.stringify({
 						action: 'mintResponse',
 						status: 'ok'
 					}));
-        } else {
-          ws.send(JSON.stringify({
+				} else {
+					ws.send(JSON.stringify({
 						action: 'mintResponse',
 						status: 'denied'
 					}));
-          console.log(`RECORDS, WARNING: UNAUTHORIZED USER ${name} ATTEMPTS TO MINT ${message.amount}`);
-        }
+					console.log(`RECORDS, WARNING: UNAUTHORIZED USER ${name} ATTEMPTS TO MINT ${message.amount}`);
+				}
 
-        break;
-      }
+				break;
+			}
 
 			case 'voidCoin': {
-        if (message.amount !== parseInt(message.amount, 10)) {
-          ws.send(JSON.stringify({
+				if (message.amount !== parseInt(message.amount, 10)) {
+					ws.send(JSON.stringify({
 						action: 'voidResponse',
 						status: 'badInput'
 					}));
-        } else if (admin) {
+				} else if (admin) {
 					await db.addTransaction(userID, 'nobody', message.amount);
-          ws.send(JSON.stringify({
+					ws.send(JSON.stringify({
 						action: 'voidResponse',
 						status: 'ok'
 					}));
-        } else {
-          ws.send(JSON.stringify({
+				} else {
+					ws.send(JSON.stringify({
 						action: 'voidResponse',
 						status: 'denied'
 					}));
-          console.log(`RECORDS, WARNING: UNAUTHORIZED USER ${name} ATTEMPTS TO VOID ${message.amount}`);
-        }
+					console.log(`RECORDS, WARNING: UNAUTHORIZED USER ${name} ATTEMPTS TO VOID ${message.amount}`);
+				}
 
-        break;
-      }
+				break;
+			}
 
 			case 'getClasses': {
 				var result = await google.getCourses(classroomAPI);
-        if (result.err) {
-          ws.send(JSON.stringify({
+				if (result.err) {
+					ws.send(JSON.stringify({
 						action: 'getClassesResponse',
 						status: 'ServerError',
-            err: result.err
+						err: result.err
 					}));
-        } else {
+				} else {
 					const { courses } = result.res.data;
-          if (courses && courses.length) {
-            ws.send(JSON.stringify({
+					if (courses && courses.length) {
+						ws.send(JSON.stringify({
 							action: 'getClassesResponse',
 							status: 'ok',
-              classes: courses
+							classes: courses
 						}));
-          }
-        }
+					}
+				}
 
-        break;
-      }
+				break;
+			}
 
 			case 'getStudents': {
 				var result = await google.getStudents(classroomAPI, message.classID);
-        if (result.err) {
-          ws.send(JSON.stringify({
+				if (result.err) {
+					ws.send(JSON.stringify({
 						action: 'getStudentsResponse',
 						status: 'ServerError',
-            err: result.err
+						err: result.err
 					}));
-        } else {
-          console.log(result.res.data);
+				} else {
+					console.log(result.res.data);
 					const { students } = result.res.data;
-          ws.send(JSON.stringify({
+					ws.send(JSON.stringify({
 						action: 'getStudentsResponse',
 						status: 'ok',
 						students
 					}));
-        }
+				}
 
-        break;
-      }
+				break;
+			}
 
 			case 'oauthInfo': { }
 			case 'elevate': {
-        if (!admin || !conf.enableRemote) {
-          console.log(`RECORDS, WARNING: UNAUTHORIZED USER ${name} ATTEMPTS ELEVATED ACTION ${message.procedure} WITH BODY ${message.body}`);
-          ws.send(JSON.stringify({
+				if (!admin || !conf.enableRemote) {
+					console.log(`RECORDS, WARNING: UNAUTHORIZED USER ${name} ATTEMPTS ELEVATED ACTION ${message.procedure} WITH BODY ${message.body}`);
+					ws.send(JSON.stringify({
 						action: 'elevateResult',
 						status: 'denied'
 					}));
-        } else {
-          console.log(`RECORDS, LOGGING: USER ${name} EXECUTES ELEVATED ACTION ${message.procedure} WITH BODY ${message.body}`);
+				} else {
+					console.log(`RECORDS, LOGGING: USER ${name} EXECUTES ELEVATED ACTION ${message.procedure} WITH BODY ${message.body}`);
 					ad.handle(message, ws).catch(error => {
-            ws.send(JSON.stringify({
+						ws.send(JSON.stringify({
 							action: 'elevateResult',
 							status: 'error',
 							contents: error
 						}));
 					});
-        }
+				}
 
-        break;
-      }
+				break;
+			}
 
 			case 'pong': { break; }
-      default:
-        console.error(`RECORDS, WARNING: User ${name} attempts invalid action ${message.action}.`);
-    }
+			default:
+				console.error(`RECORDS, WARNING: User ${name} attempts invalid action ${message.action}.`);
+		}
 	});
-  ws.on('close', async () => {
-    console.log(`RECORDS, LOGGING: User ${name} has disconnected.`);
+	ws.on('close', async () => {
+		console.log(`RECORDS, LOGGING: User ${name} has disconnected.`);
 	});
 }
 
