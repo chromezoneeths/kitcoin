0d573e190b4191a66f31ba80d91570eb6307272d
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-07 13:54:11 -0500

    Changed const name for consistency

--- src/routes/oauth.ts
+++ src/routes/oauth.ts
@@ -1,25 +1,25 @@
 import * as express from 'express';
 import * as fs from 'fs';
-const oauthRouter = express.Router();
+const router = express.Router();
 
 import * as google from '../google';
 
-oauthRouter.get('/oauthstage1', (request, response) => {
+router.get('/oauthstage1', (request, response) => {
 	google.callback(request, response, '/oauthstage1');
 });
-oauthRouter.get('/oauthstage2', (request, response) => {
+router.get('/oauthstage2', (request, response) => {
 	google.callback(request, response, '/oauthstage2');
 });
-oauthRouter.get('/oauthstage3', (request, response) => {
+router.get('/oauthstage3', (request, response) => {
 	console.log(request.url);
 	google.callback(request, response, request.url);
 });
-oauthRouter.get('/stage1.js', (request, response) => {
+router.get('/stage1.js', (request, response) => {
 	response.end(fs.readFileSync('clientJs/stage1.js'));
 });
-oauthRouter.get('/stage2.js', (request, response) => {
+router.get('/stage2.js', (request, response) => {
 	response.end(fs.readFileSync('clientJs/stage2.js'));
 });
 
-export default oauthRouter;
+export default router;
 
