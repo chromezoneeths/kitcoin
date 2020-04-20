d47f779785f53e4a48c0e00243f0677d033f5216
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-20 09:24:23 -0500

    these tests are wack

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
 const del = require('del');
 const path = require('path');
-const jest = require('gulp-jest').default;
 const compose = require('docker-compose');
 
 gulp.task('default', () => {
@@ -12,25 +11,3 @@ gulp.task('default', () => {
 		.js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean-tests', ()=>{
-	return del('test_data/**');
-})
-
-gulp.task('test-up', ()=>{
-	return compose.upAll({
-		cwd: path.join(__dirname),
-		log: true,
-		composeOptions: ['-f', './docker-compose.test.yml'],
-		commandOptions: ['--build']
-	})
-})
-
-gulp.task('test-down', ()=>{
-	return compose.down()
-})
-
-gulp.task('jest', ()=>{
-	return gulp.src('./tests').pipe(jest())
-})
-
-gulp.task('test', gulp.series('clean-tests', 'test-up', 'jest', 'test-down'))
