407ea48cc4abfa27905e92f9ba97b9fa32407230
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-27 15:37:17 -0500

    Added SSR

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
+const gulpCopy = require('gulp-copy');
 
-gulp.task('default', () => {
+gulp.task('ts', () => {
 	return tsProject.src()
 		.pipe(tsProject())
 		.js.pipe(gulp.dest('dist'));
 });
+gulp.task('ejs', () => {
+	return gulp.src('./views/**')
+		.pipe(gulpCopy('./dist'));
+});
+gulp.task('static', () => {
+	return gulp.src('./static/**')
+		.pipe(gulpCopy('./dist'));
+});
+
+gulp.task('default', gulp.parallel(['ts', 'ejs', 'static']));
 
