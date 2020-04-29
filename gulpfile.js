cd704e3a09f1215a2b893f3a0888dcc70ff8e284
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-29 10:50:08 -0500

    Added a restart job to automatically update the server

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const tsProject = ts.createProject('tsconfig.json');
 const gulpCopy = require('gulp-copy');
 const {watch} = require('gulp');
 const xo = require('gulp-xo');
+const {exec} = require('child_process');
 
 gulp.task('ts', () => {
 	return tsProject.src()
@@ -26,11 +27,29 @@ gulp.task('lint', () => {
 			console.log(`Linting finished with ${results.warningCount} warnings and ${results.errorCount} errors.`);
 		}));
 });
+gulp.task('restart', async cb => {
+	const up = await new Promise(resolve => {
+		exec('docker-compose top', (err, stdout) => {
+			if (err) {
+				throw err;
+			}
 
-gulp.task('default', gulp.parallel(['ts', 'ejs', 'static']));
+			resolve(stdout.length > 1);
+		});
+	});
+	if (up) {
+		console.log('Kitcoin is running, rebuilding to apply changes…');
+		exec('docker-compose up --build -d', cb);
+	} else {
+		console.log('Kitcoin isn’t running, so we won’t start it.');
+		cb();
+	}
+});
+
+gulp.task('default', gulp.parallel([gulp.series('ts', 'restart'), 'ejs', 'static']));
 
 gulp.task('watch', () => {
-	watch('src/**.ts', gulp.series('lint', 'ts'));
+	watch('src/**.ts', gulp.series('lint', 'ts', 'restart'));
 	watch('views/**', gulp.series('lint', 'ejs'));
 	watch('./static/**', gulp.series('lint', 'static'));
 });
