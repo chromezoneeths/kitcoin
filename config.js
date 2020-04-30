ada4f122f087d8d6f4c58d807b2ada3808ec9019
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-04-30 10:11:40 -0500

    Cleaned out old stuff

--- config.js
+++
@@ -1,8 +0,0 @@
-// This file allows reading config values from environment variables or using a default.
-
-exports.nodePort = process.env.PORT || 5000;
-exports.dbAddress = process.env.MONGO_ADDR || 'db';
-exports.dbUser = process.env.MONGO_USER || 'kcuser';
-exports.dbPass = process.env.MONGO_PASS || 'kcpass';
-exports.dbName = process.env.MONGO_DBNAME || 'kitcoin';
-exports.baseUrl = process.env.EXTERNAL_URL || 'https://kitcoin.eths.k12.il.us';
