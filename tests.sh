72e3909ce59f9b7b5bebf9925f240f0a5033aec6
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-06-03 20:14:59 -0500

    Changed test script to preserve exit code

--- tests.sh
+++ tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker-compose -f docker-compose.yml -f docker-compose.test.yml build
-docker-compose -f docker-compose.yml -f docker-compose.test.yml up test
+docker-compose -f docker-compose.yml -f docker-compose.test.yml up --exit-code-from test test
+EXIT=$?
 docker-compose -f docker-compose.yml -f docker-compose.test.yml down
+exit $EXIT
