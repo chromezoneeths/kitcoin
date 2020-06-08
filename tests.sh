3307564a5413aa3581b2694c81613945e2452057
Tucker Carlson-Huber <tuckerch@tuta.io>
2020-06-08 10:31:57 -0500

    Added a message when tests pass

--- tests.sh
+++ tests.sh
@@ -3,4 +3,5 @@ docker-compose -f docker-compose.yml -f docker-compose.test.yml build
 docker-compose -f docker-compose.yml -f docker-compose.test.yml up --exit-code-from test test
 EXIT=$?
 docker-compose -f docker-compose.yml -f docker-compose.test.yml down
+test [$EXIT="0"] && echo "Test passed"
 exit $EXIT
